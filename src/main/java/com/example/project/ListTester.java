78bdcdbd2999937e80a58c761cb731723dd98851
abdulrahman-almanea <444102607@student.ksu.edu.sa>
2024-09-14 23:50:12 +0300

    Update ListTester.java

--- src/main/java/com/example/project/ListTester.java
+++ src/main/java/com/example/project/ListTester.java
@@ -4,14 +4,17 @@ public class ListTester {
     public static <T> void circularLeftShift(List <T> list, int n)
     {
       
-    	 if ( list.empty() || n < 1) return;
+     if ( list.empty() || n < 1) return;
     	 for (int i = 0; i < n; i++) {
-    		  list.findFirst();
-    		 	T temp =list.retrieve() ;
-    	        list.insert(temp);
+    		   list.findFirst();
+    		   T copy = list.retrieve();
+    		   while(!list.last()) {
+    		   list.findNext();
+    		   }
+    	        list.insert(copy);
     		 	list.findFirst();
     	        list.remove();
-             list.findFirst();
+    	      
     	    }
     }
     public static <T> void reverseCopy(DLL<T> l1, DLL<T> l2)
